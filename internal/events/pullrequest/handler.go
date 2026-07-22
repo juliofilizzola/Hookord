c8feb566c29775bc4f930d65155dfc744a3beb53
julio_filizzola <juliofilizzola@hotmail.com>
2026-07-22 19:37:21 -0300

    feat: improve pull request event handling with notifications and embed details
    details:
    - add `DetectType` function to classify pull request titles
    - introduce reviewer and `@everyone` notifications for specific scenarios
    - enhance embed with stats, assignees, reviewers, labels, and repository owner avatar

--- internal/events/pullrequest/handler.go
+++ internal/events/pullrequest/handler.go
@@ -3,6 +3,8 @@ package pullrequest
 import (
 	"context"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/google/go-github/v60/github"
@@ -27,8 +29,25 @@ func Handle(ctx context.Context, payload *EventPayload, repo domain.MessageRepos
 
 	embed := BuildEmbed(payload)
 
+	reviewers := []string{}
+	for _, reviewer := range payload.PullRequest.RequestedReviewers {
+		reviewers = append(reviewers, "@"+reviewer.GetLogin())
+	}
+
+	content := ""
+	if DetectType(payload.PullRequest.GetTitle()) == domain.TypeHot && payload.PullRequest.GetState() == domain.PullRequestStateOpen && !payload.PullRequest.GetDraft() {
+		content = "@everyone"
+	}
+
+	if len(reviewers) > 0 && payload.PullRequest.GetState() == domain.PullRequestStateOpen {
+		if content != "" {
+			content += " "
+		}
+		content += strings.Join(reviewers, " ")
+	}
+
 	if mapping == nil {
-		msgID, err := discord.SendMessage(ctx, channelID, embed)
+		msgID, err := discord.SendMessage(ctx, channelID, content, embed)
 		if err != nil {
 			return err
 		}
@@ -62,6 +81,37 @@ func BuildEmbed(payload *EventPayload) *discordgo.MessageEmbed {
 		color = colors.Green
 	}
 
+	if DetectType(pr.GetTitle()) == domain.TypeHot {
+		color = colors.Red
+	}
+
+	var reviewers []string
+	for _, r := range pr.RequestedReviewers {
+		reviewers = append(reviewers, r.GetLogin())
+	}
+	reviewersStr := "None"
+	if len(reviewers) > 0 {
+		reviewersStr = strings.Join(reviewers, ", ")
+	}
+
+	var labels []string
+	for _, l := range pr.Labels {
+		labels = append(labels, l.GetName())
+	}
+	labelsStr := "None"
+	if len(labels) > 0 {
+		labelsStr = strings.Join(labels, ", ")
+	}
+
+	var assignees []string
+	for _, a := range pr.Assignees {
+		assignees = append(assignees, a.GetLogin())
+	}
+	assigneesStr := "None"
+	if len(assignees) > 0 {
+		assigneesStr = strings.Join(assignees, ", ")
+	}
+
 	embed := &discordgo.MessageEmbed{
 		Title:       "Pull Request #" + strconv.Itoa(pr.GetNumber()) + " - " + pr.GetTitle(),
 		URL:         pr.GetHTMLURL(),
@@ -80,6 +130,26 @@ func BuildEmbed(payload *EventPayload) *discordgo.MessageEmbed {
 			{
 				Name:   "Repository",
 				Value:  payload.Repository.GetFullName(),
+				Inline: false,
+			},
+			{
+				Name:   "Stats",
+				Value:  "++" + strconv.Itoa(pr.GetAdditions()) + " --" + strconv.Itoa(pr.GetDeletions()),
+				Inline: true,
+			},
+			{
+				Name:   "Reviewers",
+				Value:  reviewersStr,
+				Inline: true,
+			},
+			{
+				Name:   "Assignees",
+				Value:  assigneesStr,
+				Inline: true,
+			},
+			{
+				Name:   "Labels",
+				Value:  labelsStr,
 				Inline: true,
 			},
 			{
@@ -88,10 +158,32 @@ func BuildEmbed(payload *EventPayload) *discordgo.MessageEmbed {
 				Inline: false,
 			},
 		},
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: payload.Repository.GetOwner().GetAvatarURL(),
+		},
 		Footer: &discordgo.MessageEmbedFooter{
 			Text: "GitHub ↔ Discord Notification Hookord",
+			//IconURL: "https://raw.githubusercontent.com/juliofiliizzola/hookord/main/asserts/hookord_github.2.png",
 		},
+		Timestamp: time.Now().UTC().Format(time.RFC3339),
 	}
 
 	return embed
+}
+
+func DetectType(title string) string {
+	title = strings.ToLower(title)
+	if strings.Contains(title, "fix") {
+		return domain.TypeFix
+	}
+	if strings.Contains(title, "hot") {
+		return domain.TypeHot
+	}
+	if strings.Contains(title, "doc") {
+		return domain.TypeDoc
+	}
+	if strings.Contains(title, "chore") {
+		return domain.TypeChore
+	}
+	return domain.TypeOther
 }
