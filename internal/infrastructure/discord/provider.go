da5d0ea85ed9f4c4ebbe2d7768d4bf904ff11628
julio_filizzola <juliofilizzola@hotmail.com>
2026-07-22 17:00:35 -0300

    feat: add Discord provider for message handling
    details:
    - implement `NewProvider` to initialize Discord session
    - add support for sending messages with `SendMessage`
    - introduce `EditMessage` for modifying existing messages

---
+++ internal/infrastructure/discord/provider.go
@@ -0,0 +1,46 @@
+package discord
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/juliofiliizzola/hookord/internal/domain"
+)
+
+type provider struct {
+	session *discordgo.Session
+}
+
+func NewProvider(token string) (domain.DiscordProvider, error) {
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		return nil, err
+	}
+
+	return &provider{session: dg}, nil
+}
+
+func (p *provider) SendMessage(ctx context.Context, channelID string, embed interface{}) (string, error) {
+	e, ok := embed.(*discordgo.MessageEmbed)
+	if !ok {
+		return "", fmt.Errorf("invalid embed type")
+	}
+
+	msg, err := p.session.ChannelMessageSendEmbed(channelID, e)
+	if err != nil {
+		return "", err
+	}
+
+	return msg.ID, nil
+}
+
+func (p *provider) EditMessage(ctx context.Context, channelID string, messageID string, embed interface{}) error {
+	e, ok := embed.(*discordgo.MessageEmbed)
+	if !ok {
+		return fmt.Errorf("invalid embed type")
+	}
+
+	_, err := p.session.ChannelMessageEditEmbed(channelID, messageID, e)
+	return err
+}
