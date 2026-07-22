bd49f132ba8d0ddd3b5508c137d2aa5c79e9d9b7
julio_filizzola <juliofilizzola@hotmail.com>
2026-07-22 17:11:03 -0300

    feat: add constants for pull request and issue states
    details:
    - define constants for pull request states: open, closed, merged, and draft
    - define constants for issue states: open and closed

---
+++ internal/domain/status.go
@@ -0,0 +1,13 @@
+package domain
+
+const (
+	PullRequestStateOpen   = "open"
+	PullRequestStateClosed = "closed"
+	PullRequestStateMerged = "merged"
+	PullRequestStateDraft  = "draft"
+)
+
+const (
+	IssueStateOpen   = "open"
+	IssueStateClosed = "closed"
+)
