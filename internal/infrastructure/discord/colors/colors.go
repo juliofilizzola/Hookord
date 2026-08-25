e45fd44fc924e624ce1a5163077b91175e619564
julio_filizzola <juliofilizzola@hotmail.com>
2026-08-25 11:30:17 -0300

    feat: add new color constants for Discord integration
    details:
    - add Yellow and DarkGrey to the color palette
    - extend color options for Discord-related features

--- internal/infrastructure/discord/colors/colors.go
+++ internal/infrastructure/discord/colors/colors.go
@@ -1,10 +1,12 @@
 package colors
 
 const (
-	Orange = 0xe67e22
-	Grey   = 0x95a5a6
-	Green  = 0x2ecc71
-	Blue   = 0x3498db
-	Purple = 0x9b59b6
-	Red    = 0xe74c3c
+	Orange   = 0xe67e22
+	Grey     = 0x95a5a6
+	Green    = 0x2ecc71
+	Blue     = 0x3498db
+	Purple   = 0x9b59b6
+	Red      = 0xe74c3c
+	Yellow   = 0xf1c40f
+	DarkGrey = 0x7f8c8d
 )
