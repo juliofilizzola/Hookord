ceb56cc98fafbdbb4c752a373cc8cf4689ab6f13
julio_filizzola <juliofilizzola@hotmail.com>
2026-09-04 10:42:02 -0300

    refactor: remove redundant comments in DiscordClient interface

--- internal/integrations/discord/client.go
+++ internal/integrations/discord/client.go
@@ -4,8 +4,6 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-// DiscordClient defines the interface for interacting with the Discord API.
-// This interface allows mocking Discord communication in unit tests without making real network calls.
 type DiscordClient interface {
 	ChannelMessageSendComplex(channelID string, data *discordgo.MessageSend) (*discordgo.Message, error)
 	ChannelMessageEditEmbed(channelID string, messageID string, embed *discordgo.MessageEmbed) (*discordgo.Message, error)
@@ -16,7 +14,6 @@ type sessionClient struct {
 	session *discordgo.Session
 }
 
-// NewSessionClient creates and opens a new Discord session client using the bot token.
 func NewSessionClient(token string) (DiscordClient, error) {
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
