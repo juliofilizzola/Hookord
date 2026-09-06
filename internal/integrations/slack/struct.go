4cdc3fa381a64d71c2b8b55cb70c5d2070741cba
julio_filizzola <juliofilizzola@hotmail.com>
2026-09-05 22:30:38 -0300

    feat: add Slack integration struct and initial methods

---
+++ internal/integrations/slack/struct.go
@@ -0,0 +1,23 @@
+package slack
+
+import (
+	"context"
+
+	"github.com/juliofiliizzola/hookord/internal/domain"
+	"github.com/juliofiliizzola/hookord/internal/integrations"
+)
+
+type Integration struct {
+	client SlackClient
+	repo   domain.MessageRepository
+	cfg    Config
+}
+
+func (integration *Integration) Name() string {
+	return "slack"
+}
+
+func (integration *Integration) HandleIssue(ctx context.Context, event *integrations.IssueEvent) error {
+	//TODO implement me
+	return nil
+}
