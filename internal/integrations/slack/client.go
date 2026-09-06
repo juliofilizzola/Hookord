5bf5d4c88b6a16db44d080c7be86e1f7eb047d49
julio_filizzola <juliofilizzola@hotmail.com>
2026-09-05 22:30:45 -0300

    feat: implement Slack client with message posting functionality

---
+++ internal/integrations/slack/client.go
@@ -0,0 +1,27 @@
+package slack
+
+import "github.com/slack-go/slack"
+
+type SlackClient interface {
+	PostMessage(channelId string, data slack.Attachment) (string, string, error)
+}
+
+type sessionClientSlack struct {
+	session *slack.Client
+}
+
+func NewSessionClient(token string) (SlackClient, error) {
+	session := slack.New(token, slack.OptionDebug(true))
+
+	if session == nil {
+		return nil, ErrSessionConnect
+	}
+
+	return &sessionClientSlack{
+		session: session,
+	}, nil
+}
+
+func (sl *sessionClientSlack) PostMessage(channelId string, data slack.Attachment) (string, string, error) {
+	return sl.session.PostMessage(channelId, slack.MsgOptionAttachments(data))
+}
