cee80496f1b5f9a3aca6c01350e5e323094ebdc5
Julio Filizzola <juliofilizzola@hotmail.com>
2026-09-08 08:07:54 -0300

    feat: add ColorToHex function and text color constants for improved color handling

--- internal/common/utils/colors.go
+++ internal/common/utils/colors.go
@@ -1,5 +1,7 @@
 package utils
 
+import "fmt"
+
 const (
 	ColorOrange   = 0xe67e22
 	ColorGrey     = 0x95a5a6
@@ -9,4 +11,19 @@ const (
 	ColorRed      = 0xe74c3c
 	ColorYellow   = 0xf1c40f
 	ColorDarkGrey = 0x7f8c8d
+)
+
+func ColorToHex(colorCode int) string {
+	return fmt.Sprintf("#%06x", colorCode)
+}
+
+var (
+	ColorOrangeText   = ColorToHex(ColorOrange)
+	ColorGreyText     = ColorToHex(ColorGrey)
+	ColorGreenText    = ColorToHex(ColorGreen)
+	ColorBlueText     = ColorToHex(ColorBlue)
+	ColorPurpleText   = ColorToHex(ColorPurple)
+	ColorRedText      = ColorToHex(ColorRed)
+	ColorYellowText   = ColorToHex(ColorYellow)
+	ColorDarkGreyText = ColorToHex(ColorDarkGrey)
 )
