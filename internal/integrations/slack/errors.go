7f88f6dfce2fdd390c358225881bc9056921ce79
Julio Filizzola <juliofilizzola@hotmail.com>
2026-09-08 16:30:03 -0300

    feat: add ErrEventNotFound error for handling missing events in Slack API

--- internal/integrations/slack/errors.go
+++ internal/integrations/slack/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrMissingToken   = errors.New("slack: token is required")
 	ErrSessionConnect = errors.New("slack: err in connect slack api")
 	ErrEventNil       = errors.New("slack: event is required")
+	ErrEventNotFound  = errors.New("slack: event not found")
 )
